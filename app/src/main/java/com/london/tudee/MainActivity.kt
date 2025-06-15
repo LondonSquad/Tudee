67711ad18b66547c3f49124e7b8c626a47e0b3e5
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-06-15 20:16:37 +0300

    fix: error in old theme import

--- app/src/main/java/com/london/tudee/MainActivity.kt
+++ app/src/main/java/com/london/tudee/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.london.tudee.ui.theme.TudeeTheme
+import com.london.tudee.presentation.design_system.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
