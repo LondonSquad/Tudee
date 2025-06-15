a916954492205c9a05ae3e2780f1800572937b79
Abdulrahman Ragab <abdulrahman.ragab.01@gmail.com>
2025-06-15 22:52:12 +0300

    comp: add text field

--- app/src/main/java/com/london/tudee/presentation/components/TudeeTextField.kt
+++ app/src/main/java/com/london/tudee/presentation/components/TudeeTextField.kt
@@ -1,1 +1,135 @@
 package com.london.tudee.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.london.tudee.R
+import com.london.tudee.presentation.design_system.theme.LocalTudeeColors
+import com.london.tudee.presentation.design_system.theme.LocalTudeeShape
+import com.london.tudee.presentation.design_system.theme.LocalTudeeTypography
+
+@Composable
+fun TudeeTextField(
+    modifier: Modifier = Modifier,
+    icon: Int? = null,
+    hint: String,
+    multiLined: Boolean = false
+){
+    var text by rememberSaveable { mutableStateOf("") }
+
+    val focusRequester = remember { FocusRequester() }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val cornerRadius = LocalTudeeShape.current.small
+    val outlineColor = LocalTudeeColors.current.stroke
+    val focusedBorderColor =  LocalTudeeColors.current.primary
+    val borderColor = if (isFocused) focusedBorderColor else outlineColor
+
+    val iconColor = if (text.isNotEmpty()) LocalTudeeColors.current.body else LocalTudeeColors.current.hint
+
+    Box(
+        modifier = modifier
+            .background(LocalTudeeColors.current.surface)
+            .fillMaxWidth()
+            .height(if (multiLined) 168.dp else 50.dp)
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = cornerRadius,
+            )
+            .clip(cornerRadius)
+            .padding(top = if (multiLined) 12.dp else 0.dp)
+            .clickable {
+                focusRequester.requestFocus()
+            },
+        contentAlignment = if(multiLined) Alignment.TopCenter else Alignment.Center,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            icon?.let {
+                Icon(
+                    painter = painterResource(id = it),
+                    contentDescription = "icon",
+                    tint = iconColor,
+                    modifier = Modifier
+                        .padding(start = 12.dp, end = 12.dp)
+                        .size(24.dp)
+                )
+
+                VerticalDivider(
+                    modifier = Modifier
+                        .width(1.dp)
+                        .height(30.dp)
+                        .background(outlineColor)
+                )
+            }
+
+            Spacer(modifier = modifier.width(12.dp))
+
+            BasicTextField(
+                value = text,
+                onValueChange = { text = it },
+                singleLine = !multiLined,
+                interactionSource = interactionSource,
+                decorationBox = { innerTextField ->
+                    if (text.isEmpty()) {
+                        Text(
+                            text = hint,
+                            color = LocalTudeeColors.current.hint,
+                            style = LocalTudeeTypography.current.labelMedium
+                        )
+                    }
+                    innerTextField()
+                },
+                textStyle = LocalTudeeTypography.current.bodyMedium
+                    .copy(color = LocalTudeeColors.current.body),
+                maxLines = if (multiLined) Int.MAX_VALUE else 1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .padding(end = 12.dp),
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TudeeTextFieldPreview(){
+    TudeeTextField(
+        multiLined = true,
+        icon = R.drawable.add_date_icon,
+        hint = "Task Title",
+    )
+}
