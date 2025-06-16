b155ef590ad33d755273117d3fe50a7d84c6ed4b
Bassant Mohammed <bassant.22230@gmail.com>
2025-06-16 17:21:30 +0300

    comp: - implement slider and snack bar components

--- app/src/main/java/com/london/tudee/MainActivity.kt
+++ app/src/main/java/com/london/tudee/MainActivity.kt
@@ -3,13 +3,12 @@ package com.london.tudee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.london.tudee.ui.theme.TudeeTheme
+import com.london.tudee.presentation.design_system.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContent {
             TudeeTheme {
 
