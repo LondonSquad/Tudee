67ca6054182b1bdd2d120b2220a96c8f16b7cb44
abdo-essam <abdo.e.mosa@gmail.com>
2025-06-17 00:31:02 +0300

    feat: Improve DateItem text visibility
    Updated DateItem's day of week text style to `labelMedium`
    to improve readability. Also, removed unused `app_name` string
    resource.

--- app/src/main/java/com/london/tudee/presentation/components/DateItem.kt
+++ app/src/main/java/com/london/tudee/presentation/components/DateItem.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -55,7 +54,7 @@ fun DateItem(
 
         Text(
             text = dayOfWeek,
-            style = TudeeTheme.typography.labelSmall,
+            style = TudeeTheme.typography.labelMedium,
             color = if (isSelected) TudeeTheme.colors.caption else TudeeTheme.colors.hint
         )
     }
