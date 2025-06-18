98eadbb8a7404cf88f5497fd09b56c77ce35e17e
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-18 20:17:38 +0300

    fix: class Category

--- app/src/main/java/com/london/tudee/data/local/room_db/defaultCategory.kt
+++ app/src/main/java/com/london/tudee/data/local/room_db/defaultCategory.kt
@@ -1,93 +1,93 @@
 package com.london.tudee.data.local.room_db
 
 import com.london.tudee.R
-import com.london.tudee.data.local.room_db.entities.CategoryDto
+import com.london.tudee.domain.entities.Category
 
 
 val defaultCategory = listOf(
-    CategoryDto(
+    Category(
         id = 0, name = "Education",
         iconPath = R.drawable.ic_education.toString(),
         arName = "التعليم",
         isDefault = true,
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Shopping",
         arName = "التسوق",
         isDefault = false,
         iconPath = R.drawable.ic_shopping.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Medical",
         arName = "الطب",
         isDefault = false,
         iconPath = R.drawable.ic_medical.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0, name = "Gym",
         iconPath = R.drawable.ic_gym.toString(),
         arName = "التمرين",
         isDefault = false,
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Entertainment",
         arName = "الترفيه",
         isDefault = false,
         iconPath = R.drawable.ic_entertainment.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Cooking",
         arName = "الطب",
         isDefault = false,
         iconPath = R.drawable.ic_cooking.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Family & friend",
         arName = "الأسرة والاصدقاء",
         isDefault = false,
         iconPath = R.drawable.ic_family.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Traveling",
         arName = "السفر",
         isDefault = false,
         iconPath = R.drawable.ic_travel.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Agriculture",
         arName = "الزراعة",
         isDefault = false,
         iconPath = R.drawable.ic_agriculture.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Coding",
         arName = "البرمجة",
         isDefault = false,
         iconPath = R.drawable.ic_coding.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Adoration",
         arName = "الاعزاء",
         isDefault = false,
         iconPath = R.drawable.ic_adoration.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Fixing bugs",
         arName = "التصليح",
         isDefault = false,
         iconPath = R.drawable.ic_bug_fix.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Cleaning",
         arName = "التنظيف",
         isDefault = false,
         iconPath = R.drawable.ic_cleaning.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Work",
         arName = "العمل",
         isDefault = false,
         iconPath = R.drawable.ic_work.toString()
-    ), CategoryDto(
+    ), Category(
         id = 0,
         name = "Budgeting",
         arName = "الحسابات",
--- app/src/main/java/com/london/tudee/domain/services/Services.kt
+++ app/src/main/java/com/london/tudee/domain/services/Services.kt
@@ -3,7 +3,7 @@ package com.london.tudee.domain.services
 import kotlinx.coroutines.flow.Flow
 
 interface Services<T> {
-   suspend fun add(service: T) : Boolean
+    suspend fun add(service: T) : Boolean
     suspend fun edit(service: T) : Boolean
     suspend fun delete(service: T) : Boolean
     suspend fun getAll(): Flow<List<T>>
