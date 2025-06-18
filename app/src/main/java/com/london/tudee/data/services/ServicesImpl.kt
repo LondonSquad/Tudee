52a1d7385d942cd31118e27781396d7838ba661a
Bassant Mohammed <bassant.22230@gmail.com>
2025-06-18 17:13:14 +0300

    refactor: naming and add new function in interface and its implementation

--- app/src/main/java/com/london/tudee/data/services/ServicesImpl.kt
+++ app/src/main/java/com/london/tudee/data/services/ServicesImpl.kt
@@ -4,16 +4,39 @@ import com.london.tudee.domain.entities.Category
 import com.london.tudee.domain.entities.Priority
 import com.london.tudee.domain.entities.Task
 import com.london.tudee.domain.entities.TaskStatus
-import com.london.tudee.domain.services.TasksServices
+import com.london.tudee.domain.services.Services
 import java.util.concurrent.atomic.AtomicInteger
 
-class TaskServicesImpl : TasksServices {
-
+class ServicesImpl : Services {
     private val taskIdCounter = AtomicInteger(0)
     private val categoryIdCounter = AtomicInteger(0)
 
     private val tasks = mutableListOf<Task>()
     private val categories = mutableListOf<Category>()
+
+    override fun getTasksByDate(date: Long): List<Task> {
+
+        val startOfDay = date - (date % 86400000) // 86400000 ms = 1 day
+        val endOfDay = startOfDay + 86400000 - 1
+
+        return tasks.filter { task ->
+            task.date in startOfDay..endOfDay
+        }
+    }
+
+    override fun getTasksByCategory(categoryId: Int): List<Task> {
+        return tasks.filter { it.categoryId == categoryId }
+    }
+
+    override fun changeTaskState(id: Int, newStatus: TaskStatus) {
+        val taskIndex = tasks.indexOfFirst { it.id == id }
+        if (taskIndex != -1) {
+            val task = tasks[taskIndex]
+            tasks[taskIndex] = task.copy(taskStatus = newStatus)
+        } else {
+            throw NoSuchElementException("Task with id $id not found")
+        }
+    }
 
     override fun getAllTasks(): List<Task> = tasks.toList()
 
@@ -82,12 +105,13 @@ class TaskServicesImpl : TasksServices {
         description: String? = null,
         priority: Priority = Priority.MEDIUM,
         categoryId: Int = categories.firstOrNull()?.id ?: 0,
-        status: TaskStatus = TaskStatus.TO_DO
+        status: TaskStatus = TaskStatus.TO_DO,
+        date: Long = System.currentTimeMillis()
     ): Task {
         val newTask = Task(
             id = taskIdCounter.incrementAndGet(),
             title = title,
-            date = System.currentTimeMillis(),
+            date = date,
             description = description,
             priority = priority,
             categoryId = categoryId,
--- app/src/main/java/com/london/tudee/domain/services/Services.kt
+++ app/src/main/java/com/london/tudee/domain/services/Services.kt
@@ -2,13 +2,17 @@ package com.london.tudee.domain.services
 
 import com.london.tudee.domain.entities.Category
 import com.london.tudee.domain.entities.Task
+import com.london.tudee.domain.entities.TaskStatus
 
-interface TasksServices {
+interface Services {
     fun getAllTasks(): List<Task>
     fun getTaskById(id:Int): Task
     fun createTask(): Task
     fun editTask(id:Int): Task
     fun deleteTask(id:Int): Task
+    fun getTasksByDate(date: Long): List<Task>
+    fun getTasksByCategory(categoryId: Int): List<Task>
+    fun changeTaskState(id: Int, newStatus: TaskStatus)
 
     fun getAllCategory(): List<Category>
     fun getCategoryById(id:Int): Category
