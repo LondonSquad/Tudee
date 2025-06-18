4225127a82c532c9d9ff4974801fe133eb111883
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-06-18 07:27:18 +0300

    feat: Add delete task card

--- app/src/main/java/com/london/tudee/presentation/components/task/TaskItem.kt
+++ app/src/main/java/com/london/tudee/presentation/components/task/TaskItem.kt
@@ -1,7 +1,21 @@
-package com.london.tudee.presentation.components
+package com.london.tudee.presentation.components.task
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,16 +25,18 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.london.tudee.R
+import com.london.tudee.presentation.components.DateBadge
 import com.london.tudee.presentation.components.priority.Priority
 import com.london.tudee.presentation.components.priority.PriorityBadge
 import com.london.tudee.presentation.design_system.theme.ThemePreviews
 import com.london.tudee.presentation.design_system.theme.TudeeTheme
 
+
 @Composable
 fun TaskItem(
     modifier: Modifier = Modifier,
     priority: Priority,
-    iconResId: Int,
+    @DrawableRes iconResId: Int,
     title: String,
     description: String,
     date: String? = null
@@ -90,7 +106,7 @@ private fun TaskItemContent(
         modifier = modifier
             .wrapContentHeight()
             .fillMaxWidth(0.9f)
-            .padding(start = 4.dp, end = 12.dp, bottom = 24.dp)
+            .padding(start = 4.dp, end = 12.dp, bottom = 12.dp)
     ) {
         Text(
             modifier = Modifier.padding(start = 8.dp, bottom = 2.dp),
