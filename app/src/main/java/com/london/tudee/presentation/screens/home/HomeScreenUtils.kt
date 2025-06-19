de1e4985ede92cf432274fd8d828cb79ab6e24f2
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-06-20 02:13:36 +0300

    fix: Move fun formatDate to DateFormater.kt

--- app/src/main/java/com/london/tudee/presentation/screens/home/HomeScreenUtils.kt
+++ app/src/main/java/com/london/tudee/presentation/screens/home/HomeScreenUtils.kt
@@ -18,11 +18,3 @@ object HomeScreenUtils {
         return "$day $month $year"
     }
 }
-
-fun formatDate(instant: Instant): String {
-    val date = instant.toLocalDateTime(TimeZone.currentSystemDefault()).date
-    val day = date.dayOfMonth.toString().padStart(2, '0')
-    val month = date.monthNumber.toString().padStart(2, '0')
-    val year = date.year.toString()
-    return "$day-$month-$year"
-}
