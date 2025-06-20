aa22dde28e751b70bdac9447b6c92d7af72f569e
Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
2025-06-20 04:19:24 +0300

    feature/edit-category-view-model (#55)
    * feat: implement Home Screen ui

---
+++ app/src/main/java/com/london/tudee/presentation/screens/task/view_tasks/EditTaskDetailsUiState.kt
@@ -0,0 +1,15 @@
+package com.london.tudee.presentation.screens.task.view_tasks
+
+import com.london.tudee.domain.entities.Task
+
+
+data class EditTaskDetailsState(
+    val isLoading: Boolean = true,
+    val errMessage: String? = null,
+    val allTasks: List<Task> = listOf(),
+    val doneTasks: List<Task> = listOf(),
+    val inProgressTasks: List<Task> = listOf(),
+    val toDoTasks: List<Task> = listOf(),
+)
+
+
