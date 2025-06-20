5d32e25c33d1479474acfe15300d6895e477b759
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-20 11:07:10 +0300

    feat: add onboarding preferences and view model

--- app/src/main/java/com/london/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/london/tudee/di/ViewModelModule.kt
@@ -1,11 +1,14 @@
 package com.london.tudee.di
 
 import com.london.tudee.presentation.screens.home.HomeViewModel
+import com.london.tudee.presentation.screens.onboarding.OnBoardingViewModel
+import com.london.tudee.presentation.screens.onboarding.OnboardingPreferences
 import com.london.tudee.presentation.screens.task.confirm_delete_task.ConfirmDeleteTaskViewModel
 import com.london.tudee.presentation.screens.task.task_details.TaskDetailsViewModel
 import com.london.tudee.presentation.screens.task.view_tasks.EditTaskViewModel
 import com.london.tudee.presentation.screens.tasks.TasksScreenViewModel
-import org.koin.core.module.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.scope.get
 import org.koin.dsl.module
 
 val viewModelModule = module {
@@ -15,4 +18,7 @@ val viewModelModule = module {
     viewModel { TaskDetailsViewModel(get(), get()) }
     viewModel { HomeViewModel(get(), get()) }
     viewModel { EditTaskViewModel(get(), get()) }
+    single { OnboardingPreferences(get()) }
+    viewModel { OnBoardingViewModel(get()) }
+
 }
