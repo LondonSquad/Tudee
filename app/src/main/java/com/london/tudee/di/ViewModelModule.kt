ed2bf11e4e67329cff16ad1681b88fce60a22078
Bassant Mohammed <bassant.22230@gmail.com>
2025-06-20 03:43:49 +0300

    feature: integrate EditTaskViewModel and loading states

--- app/src/main/java/com/london/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/london/tudee/di/ViewModelModule.kt
@@ -2,10 +2,12 @@ package com.london.tudee.di
 
 import com.london.tudee.presentation.screens.home.HomeViewModel
 import com.london.tudee.presentation.screens.task.confirm_delete_task.ConfirmDeleteTaskViewModel
+import com.london.tudee.presentation.screens.task.view_tasks.EditTaskViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     single { ConfirmDeleteTaskViewModel(get()) }
     viewModel { HomeViewModel(get(), get()) }
+    viewModel { EditTaskViewModel(get(), get()) }
 }
--- app/src/main/java/com/london/tudee/presentation/screens/task/view_tasks/EditTaskDetailsScreen.kt
+++ app/src/main/java/com/london/tudee/presentation/screens/task/view_tasks/EditTaskDetailsScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -23,202 +26,153 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.london.tudee.R
-import com.london.tudee.domain.entities.Priority
-import com.london.tudee.domain.entities.Task
-import com.london.tudee.domain.entities.TaskStatus
-import com.london.tudee.domain.services.CategoryService
-import com.london.tudee.presentation.components.TaskStatusSlider
 import com.london.tudee.presentation.components.TopAppBar
 import com.london.tudee.presentation.components.tabs.TabItem
-import com.london.tudee.presentation.components.tabs.TudeeTabLayout
+import com.london.tudee.presentation.components.tabs.TudeeTabLayoutWithPager
 import com.london.tudee.presentation.components.task.TaskItem
 import com.london.tudee.presentation.design_system.theme.ThemePreviews
 import com.london.tudee.presentation.design_system.theme.TudeeTheme
-import kotlinx.datetime.Instant
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun EditTaskDetails(
-    modifier: Modifier = Modifier,
+    viewModel: EditTaskViewModel = koinViewModel(),
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
+    when {
+        uiState.isLoading -> LoadingScreen(modifier = Modifier.fillMaxSize())
+        uiState.errMessage != null -> ErrorScreen(modifier = Modifier.fillMaxSize())
+        else -> EditTaskDetailsContent(state = uiState)
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Text(
+            text = "Loading...",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Text(
+            text = "There was an unexpected error",
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+
+@Composable
+fun EditTaskDetailsContent(
+    state: EditTaskDetailsState,
 ) {
     Column(
-        modifier = modifier
+        modifier = Modifier
             .background(TudeeTheme.colors.surface)
     ) {
-        TopAppBar(
-            title = R.string.coding,
-            onBackClick = {},
-            onClickAction = {},
-            modifier = Modifier,
-            navigationIcon = {
-                IconButton(
-                    onClick = it,
-                    modifier = Modifier
-                        .then(
-                            if (LocalLayoutDirection.current == LayoutDirection.Rtl)
-                                Modifier.rotate(180f)
-                            else Modifier
-                        )
-                        .border(
-                            1.dp,
-                            TudeeTheme.colors.stroke,
-                            TudeeTheme.shapes.circle
-                        )
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.back_arrow),
-                        contentDescription = stringResource(R.string.back_arrow),
-                        tint = TudeeTheme.colors.body
-                    )
-                }
-            },
-            actions = {
-                IconButton(
-                    onClick = it,
-                    modifier = Modifier
-                        .then(
-                            if (LocalLayoutDirection.current == LayoutDirection.Rtl)
-                                Modifier.rotate(180f)
-                            else Modifier
-                        )
-                        .border(
-                            1.dp,
-                            TudeeTheme.colors.stroke,
-                            TudeeTheme.shapes.circle
-                        )
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.edit_icon),
-                        contentDescription = "Edit Icon",
-                        tint = TudeeTheme.colors.body
-                    )
-                }
-            }
-        )
 
-        TudeeTabLayout(
+        TopAPPBar()
+
+        TudeeTabLayoutWithPager(
             tabs = listOf(
-                TabItem(text = R.string.In_Progress, number = 14),
-                TabItem(text = R.string.Done, number = 10),
-                TabItem(text = R.string.To_Do, number = 2),
-            ),
-            selectedIndex = 0,
-            onTabSelected = {},
-            modifier = Modifier.fillMaxWidth()
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(1.dp)
-                .background(TudeeTheme.colors.stroke)
-        )
+                TabItem(text = R.string.In_Progress, number = state.inProgressTasks.size),
+                TabItem(text = R.string.To_Do, number = state.toDoTasks.size),
+                TabItem(
+                    text = R.string.Done, number = state.doneTasks.size
+                ),
 
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(TudeeTheme.colors.surface)
-        ) {
-            LazyColumn(
+                ),
+        )
+        { page ->
+            val tasks = when (page) {
+                0 -> state.inProgressTasks
+                1 -> state.toDoTasks
+                else -> state.doneTasks
+            }
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .background(TudeeTheme.colors.surface)
             ) {
-                items(taskUiStates.size) { index ->
-                    TaskItem(
-                        modifier = Modifier,
-                        isSelected = true,
-                        task = taskUiStates[index],
-                        hasDate = true
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    items(tasks.size) { index ->
+                        TaskItem(
+                            modifier = Modifier,
+                            isSelected = true,
+                            task = state.allTasks[index],
+                            hasDate = true
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
                 }
             }
         }
     }
 }
 
-val taskUiStates = listOf(
-    Task(
-        id = 1,
-        priority = Priority.MEDIUM,
-        categoryId = R.drawable.ic_education,
-        title = "Organize Study Desk",
-        description = "Review cell structure and functions for tomorrow...",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 2,
-        priority = Priority.LOW,
-        categoryId = R.drawable.coding_icon,
-        title = "Organize Study Desk",
-        description = "",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 3,
-        priority = Priority.HIGH,
-        categoryId = R.drawable.coding_icon,
-        title = "Organize Study Desk",
-        description = "Review cell structure and functions for tomorrow...",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 4,
-        priority = Priority.MEDIUM,
-        categoryId = R.drawable.coding_icon,
-        title = "Organize Study Desk",
-        description = "Review cell structure and functions for tomorrow...",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 5,
-        priority = Priority.HIGH,
-        categoryId = R.drawable.coding_icon,
-        title = "Task 1,",
-        description = "This is a task description",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 5,
-        priority = Priority.LOW,
-        categoryId = R.drawable.coding_icon,
-        title = "Task 1,",
-        description = "This is a task description",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 6,
-        priority = Priority.MEDIUM,
-        categoryId = R.drawable.coding_icon,
-        title = "Task 1,",
-        description = "This is a task description",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 7,
-        priority = Priority.LOW,
-        categoryId = R.drawable.coding_icon,
-        title = "Task 1,",
-        description = "This is a task description",
-        taskStatus = TaskStatus.TODO,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
-    ),
-    Task(
-        id = 8,
-        priority = Priority.HIGH,
-        categoryId = R.drawable.coding_icon,
-        title = "Task 1,",
-        description = "Review cell structure and functions for tomorrow...",
-        taskStatus = TaskStatus.IN_PROGRESS,
-        timeStamp = Instant.parse("2023-09-20T00:00:00Z")
+@Composable
+private fun TopAPPBar() {
+    TopAppBar(
+        title = R.string.coding,
+        onBackClick = {},
+        onClickAction = {},
+        modifier = Modifier,
+        navigationIcon = {
+            IconButton(
+                onClick = it,
+                modifier = Modifier
+                    .then(
+                        if (LocalLayoutDirection.current == LayoutDirection.Rtl)
+                            Modifier.rotate(180f)
+                        else Modifier
+                    )
+                    .border(
+                        1.dp,
+                        TudeeTheme.colors.stroke,
+                        TudeeTheme.shapes.circle
+                    )
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.back_arrow),
+                    contentDescription = stringResource(R.string.back_arrow),
+                    tint = TudeeTheme.colors.body
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = it,
+                modifier = Modifier
+                    .then(
+                        if (LocalLayoutDirection.current == LayoutDirection.Rtl)
+                            Modifier.rotate(180f)
+                        else Modifier
+                    )
+                    .border(
+                        1.dp,
+                        TudeeTheme.colors.stroke,
+                        TudeeTheme.shapes.circle
+                    )
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.edit_icon),
+                    contentDescription = "Edit Icon",
+                    tint = TudeeTheme.colors.body
+                )
+            }
+        }
     )
-)
+}
 
 @ThemePreviews
 @Composable
@@ -227,3 +181,4 @@ private fun TudeeTaskPreview() {
         EditTaskDetails()
     }
 }
+
