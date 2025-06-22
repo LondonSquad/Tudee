75cbf3480ba3bc7cc0bea7857c2754e215206904
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-06-22 19:26:08 +0300

    refactor: implement new snackbar system
    This commit introduces a new Snackbar system with the following components:
    
    - `SnackbarController`: Manages Snackbar events using Kotlin Flows.
    - `ObserveAsEvent`: A Composable function to observe Flow events within a Composable's lifecycle.
    - `SnackBarCard`: A Composable for displaying the Snackbar UI.
    - `dropShadow`: A Modifier extension for adding drop shadows to Composables.
    - `SnackbarEvent`: A data class representing Snackbar events.
    
    The `MainActivity` has been updated to integrate this new Snackbar system, including a `SnackbarHandler` Composable to manage Snackbar display.
    
    The old `SnackBar.kt` file has been deleted.

--- app/src/main/java/com/london/tudee/presentation/MainActivity.kt
+++ app/src/main/java/com/london/tudee/presentation/MainActivity.kt
@@ -15,19 +15,30 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.london.tudee.R
+import com.london.tudee.presentation.components.snackbar.ObserveAsEvent
+import com.london.tudee.presentation.components.snackbar.SnackBarCard
+import com.london.tudee.presentation.components.snackbar.SnackbarController
 import com.london.tudee.presentation.design_system.theme.ThemePreviews
 import com.london.tudee.presentation.design_system.theme.TudeeTheme
 import com.london.tudee.presentation.screens.home.HomeScreen
 import com.london.tudee.presentation.screens.onboarding.OnBoardingHorizontalPager
 import com.london.tudee.presentation.screens.onboarding.OnBoardingViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -52,9 +63,56 @@ class MainActivity : ComponentActivity() {
                     } else HomeScreen()
                 }
             }
+            SnackbarHandler()
         }
     }
 }
+
+
+@Composable
+fun SnackbarHandler() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    ObserveAsEvent(
+        flow = SnackbarController.event,
+        onEvent = { event ->
+            coroutineScope.launch {
+                snackbarHostState.currentSnackbarData?.dismiss()
+                snackbarHostState.showSnackbar(
+                    message = event.message,
+                    duration = SnackbarDuration.Short
+                )
+            }
+        }
+    )
+
+    SnackbarHost(
+        hostState = snackbarHostState,
+        snackbar = { snackbarData ->
+            val (iconRes, iconTint) = when {
+                snackbarData.visuals.message.contains("success", ignoreCase = true) ->
+                    Pair(R.drawable.snack_bar_container, TudeeTheme.colors.greenAccent)
+
+                snackbarData.visuals.message.contains("error", ignoreCase = true) ->
+                    Pair(R.drawable.snack_bar_error, TudeeTheme.colors.error)
+
+                else ->
+                    Pair(R.drawable.snack_bar_error, TudeeTheme.colors.error)
+            }
+
+            SnackBarCard(
+                message = snackbarData.visuals.message,
+                icon = painterResource(id = iconRes),
+                iconTint = iconTint,
+                iconBackgroundColor = TudeeTheme.colors.surface,
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 48.dp)
+            )
+
+        }
+    )
+}
+
+
 
 @ThemePreviews
 @Composable
