24594317bbd8ea59b901498a4275d93692c70e95
Marwan Qashwa <100484790+marwanqashwa@users.noreply.github.com>
2025-06-22 17:20:59 +0300

    fix: move actions in task details bottom sheet (#68)
    * fix: handle actions in task details bottom sheet add dummy bottom sheet it will be edit task
    
    * fix: unify the naming
    
    * fix: rename some var solve 'it' conflict use string resource

--- app/src/main/java/com/london/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/london/tudee/di/ViewModelModule.kt
@@ -2,7 +2,7 @@ package com.london.tudee.di
 
 import com.london.tudee.presentation.screens.home.HomeViewModel
 import com.london.tudee.presentation.screens.task.confirm_delete_task.ConfirmDeleteTaskViewModel
-import com.london.tudee.presentation.screens.task.task_details.TaskDetailsViewModel
+import com.london.tudee.presentation.screens.task.taskdetails.TaskDetailsBottomSheetViewModel
 import com.london.tudee.presentation.screens.task.view_tasks.EditTaskViewModel
 import com.london.tudee.presentation.screens.tasks.TasksScreenViewModel
 import com.london.tudee.presentation.screens.categories.CategoriesViewModel
@@ -19,7 +19,7 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModelOf(::TasksScreenViewModel)
     viewModelOf(::ConfirmDeleteTaskViewModel)
-    viewModelOf(::TaskDetailsViewModel)
+    viewModelOf(::TaskDetailsBottomSheetViewModel)
     viewModelOf(::HomeViewModel)
     viewModelOf(::EditTaskViewModel)
     viewModelOf(::CategoriesViewModel)
