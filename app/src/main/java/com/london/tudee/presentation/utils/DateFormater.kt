2a73c56f59d558e68359097236400c6d7fab67df
Karim Al-Zmzmy <karam.zmzmy@gmail.com>
2025-06-23 02:17:22 +0300

    refactor: add date formater

--- app/src/main/java/com/london/tudee/presentation/utils/DateFormater.kt
+++ app/src/main/java/com/london/tudee/presentation/utils/DateFormater.kt
@@ -11,3 +11,28 @@ fun formatDate(instant: Instant): String {
     val year = date.year.toString()
     return "$day-$month-$year"
 }
+
+object DateFormatter {
+
+    private val timeZone = TimeZone.currentSystemDefault()
+
+    fun Long.toMonthShort(): String {
+        val dt = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
+        return dt.month.name.take(3).lowercase().replaceFirstChar { it.uppercase() } //  Jun
+    }
+
+    fun Long.toYear(): String {
+        val dt = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
+        return dt.year.toString() // 1999
+    }
+
+    fun Long.toDayNumber(): String {
+        val dt = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
+        return dt.dayOfMonth.toString().padStart(2, '0') // 25
+    }
+
+    fun Long.toDayOfWeekShort(): String {
+        val dt = Instant.fromEpochMilliseconds(this).toLocalDateTime(timeZone)
+        return dt.dayOfWeek.name.take(3).lowercase().replaceFirstChar { it.uppercase() } //  Mon
+    }
+}
