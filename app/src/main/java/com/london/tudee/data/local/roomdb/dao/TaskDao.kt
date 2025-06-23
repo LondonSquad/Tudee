f0ecbec9c05e703e95b75203cca9caf419c2de00
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-23 12:20:36 +0300

    enhance/data-room (#74)
    * feat: add getByTimeStampAndTaskStatus method
    
    * feat: add more features for business need and make names more descriptive

--- app/src/main/java/com/london/tudee/data/local/roomdb/dao/TaskDao.kt
+++ app/src/main/java/com/london/tudee/data/local/roomdb/dao/TaskDao.kt
@@ -26,6 +26,15 @@ interface TaskDao {
     @Query("SELECT * FROM TASK_TABLE WHERE timeStamp = :time")
     fun getTasksByDate(time: Long): Flow<List<TaskDto>>
 
+    @Query("SELECT * FROM TASK_TABLE WHERE taskStatus = :taskStatus AND timeStamp = :timeStamp")
+    fun getByTimeStampAndTaskStatus(taskStatus: TaskStatus, timeStamp: Long): Flow<List<TaskDto>>
+
+    @Query("SELECT * FROM TASK_TABLE WHERE categoryid = :categoryId AND taskStatus = :taskStatus")
+    fun getByCategoryIdAndTaskStatus(categoryId: Int, taskStatus: TaskStatus): Flow<List<TaskDto>>
+
+    @Query("SELECT * FROM TASK_TABLE WHERE (:timeStamp IS NULL OR timeStamp = :timeStamp) AND taskStatus = :taskStatus ")
+    fun getByDateAndTaskStatus(timeStamp: Long?, taskStatus: TaskStatus): Flow<List<TaskDto>>
+
     @Insert
     suspend fun insert(task: TaskDto)
 
