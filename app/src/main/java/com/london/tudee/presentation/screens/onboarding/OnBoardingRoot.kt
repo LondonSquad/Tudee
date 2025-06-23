a55cfb4db53d4dca9bc8244db8c2fd0b40fa86bd
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-23 15:29:02 +0300

    feat: update OnBoardingRoot with callbacks

--- app/src/main/java/com/london/tudee/presentation/screens/onboarding/OnBoardingRoot.kt
+++ app/src/main/java/com/london/tudee/presentation/screens/onboarding/OnBoardingRoot.kt
@@ -1,27 +1,39 @@
 package com.london.tudee.presentation.screens.onboarding
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import com.london.tudee.presentation.screens.home.HomeScreen
 import com.london.tudee.presentation.screens.splash.SplashScreen
 import dev.burnoo.compose.rememberpreference.rememberBooleanPreference
 
 @Composable
-fun OnBoardingRoot() {
+fun OnBoardingRoot(
+    onCompleted: () -> Unit,
+    onSkip: () -> Unit,
+) {
     var isOnboardingCompleted by rememberBooleanPreference(
         keyName = "onboardingKey",
         initialValue = null,
         defaultValue = false,
     )
-
     when (isOnboardingCompleted) {
         null -> SplashScreen()
         false -> OnBoardingHorizontalPager(
-            onCompleted = { isOnboardingCompleted = true },
-            onClickSkip = { isOnboardingCompleted = true },
+            onCompleted = {
+                isOnboardingCompleted = true
+                onCompleted()
+            },
+            onClickSkip = {
+                isOnboardingCompleted = true
+                onSkip()
+            },
         )
 
-        true -> HomeScreen(onArrowClicked = {})
+        true -> {
+            LaunchedEffect(Unit) {
+                onCompleted()
+            }
+        }
     }
 }
