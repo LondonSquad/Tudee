873a27b9252eaca633996f3349be937195c2c01f
Asmaa Khaled Gomaa <asmaa.kh.gomaa@gmail.com>
2025-06-24 17:01:44 +0300

    Enhance Date Badged Component (#88)
    * feat: add `DateBadgeStyleValues.kt` for styling date badges
    
    * refactor: Update DateBadge to use DateBadgeStyleValues in HomeScreen
    
    * refactor: Encapsulate DateBadge styling in TaskItem
    
    * refactor: Introduce DateBadgeStyleValues for DateBadge component

--- app/src/main/java/com/london/tudee/presentation/components/date/DateBadge.kt
+++ app/src/main/java/com/london/tudee/presentation/components/date/DateBadge.kt
@@ -3,27 +3,23 @@ package com.london.tudee.presentation.components.date
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.london.tudee.R
@@ -34,46 +30,38 @@ import com.london.tudee.presentation.design_system.theme.TudeeTheme
 fun DateBadge(
     modifier: Modifier = Modifier,
     dateText: String,
-    shape: Shape,
-    colors: CardColors,
-    iconSize: Dp,
-    textSize: TextUnit,
-    textStyle: TextStyle,
-    lineHeight: TextUnit,
-    iconColor: Color,
-    textColor: Color,
-    contentPadding: PaddingValues,
+    dateBadgeStyle: DateBadgeStyleValues,
     isVisible: Boolean
 ) {
     if (!isVisible) return
     Card(
         modifier = modifier,
-        shape = shape,
-        colors = colors
+        shape = dateBadgeStyle.shape,
+        colors = dateBadgeStyle.colors
     ) {
         Row(
             modifier = Modifier
+                .padding(dateBadgeStyle.contentPadding)
                 .wrapContentWidth()
-                .fillMaxHeight()
-                .padding(contentPadding),
+                .fillMaxHeight(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             Icon(
                 modifier = Modifier
-                    .padding(top = 2.dp, bottom = 2.dp, end = 2.dp)
-                    .size(iconSize),
+                    .padding(vertical = 2.dp)
+                    .align(Alignment.CenterVertically)
+                    .size(dateBadgeStyle.iconSize),
                 painter = painterResource(id = R.drawable.ic_calendar_favorite),
                 contentDescription = "Calendar Icon",
-                tint = iconColor
+                tint = dateBadgeStyle.iconColor
             )
+            Spacer(modifier = Modifier.width(dateBadgeStyle.iconTextSpacing))
             Text(
+                modifier = Modifier.align(Alignment.CenterVertically),
                 text = dateText,
-                style = textStyle,
-                fontSize = textSize,
-                lineHeight = lineHeight,
+                style = dateBadgeStyle.textStyle,
                 textAlign = TextAlign.Center,
-                color = textColor
             )
         }
     }
@@ -85,16 +73,19 @@ fun PreviewDateBadge() {
     TudeeTheme {
         DateBadge(
             modifier = Modifier.height(28.dp),
-            shape = TudeeTheme.shapes.circle,
-            colors = CardDefaults.cardColors(containerColor = TudeeTheme.colors.surface),
             dateText = "12-03-2025",
-            iconSize = 12.dp,
-            textSize = 13.sp,
-            textStyle = TudeeTheme.typography.labelSmall,
-            lineHeight = 16.sp,
-            iconColor = TudeeTheme.colors.body,
-            textColor = TudeeTheme.colors.body,
-            contentPadding = PaddingValues(vertical = 6.dp, horizontal = 8.dp),
+            dateBadgeStyle = DateBadgeStyleValues(
+                shape = TudeeTheme.shapes.circle,
+                colors = CardDefaults.cardColors(containerColor = TudeeTheme.colors.surface),
+                iconSize = 12.dp,
+                iconColor = TudeeTheme.colors.body,
+                textStyle = TudeeTheme.typography.labelSmall.copy(
+                    fontSize = 13.sp,
+                    lineHeight = 16.sp,
+                    color = TudeeTheme.colors.body
+                ),
+                contentPadding = PaddingValues(vertical = 6.dp, horizontal = 8.dp)
+            ),
             isVisible = true
         )
     }
@@ -106,18 +97,22 @@ fun PreviewDateBadgeHomeScreen() {
     TudeeTheme {
         DateBadge(
             modifier = Modifier.height(17.dp),
-            shape = RectangleShape,
-            colors = CardDefaults.cardColors(containerColor = TudeeTheme.colors.surfaceHigh),
             dateText = "today,12-03-2025",
-            iconSize = 16.dp,
-            textSize = 14.sp,
-            textStyle = TudeeTheme.typography.labelMedium,
-            lineHeight = 16.sp,
-            iconColor = TudeeTheme.colors.body,
-            textColor = TudeeTheme.colors.body,
-            contentPadding = PaddingValues(vertical = 0.dp, horizontal = 8.dp),
+            dateBadgeStyle = DateBadgeStyleValues(
+                shape = RectangleShape,
+                colors = CardDefaults.cardColors(containerColor = TudeeTheme.colors.surfaceHigh),
+                iconSize = 16.dp,
+                iconColor = TudeeTheme.colors.body,
+                textStyle = TudeeTheme.typography.labelMedium.copy(
+                    fontSize = 14.sp,
+                    lineHeight = 16.sp,
+                    color = TudeeTheme.colors.body
+                ),
+                contentPadding = PaddingValues(vertical = 0.dp, horizontal = 8.dp)
+            ),
             isVisible = false
         )
     }
 }
+
 
