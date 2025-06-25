2812378b9c4813bbbd5ca71039dd24a58377aedc
Zeyad Abdullah <90288511+ZeyadAbdullah679@users.noreply.github.com>
2025-06-25 03:42:21 +0300

    refactor: fix padding in bottom sheet components (#96)
    * refactor: fix padding in bottom sheet components
    
    * refactor: remove unused annotation in MainActivity

--- app/src/main/java/com/london/tudee/presentation/components/bottom_sheet/TudeeBottomSheetScreen.kt
+++ app/src/main/java/com/london/tudee/presentation/components/bottom_sheet/TudeeBottomSheetScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,7 +56,6 @@ fun TudeeBottomSheetScreen(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .navigationBarsPadding()
     ) {
         screenContent()
 
