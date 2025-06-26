c16a7aca2830732ef385ac9d00db11bc8b8a69e1
Abdulrahman Mohammad Ragab <76666355+Abdulrahman1890@users.noreply.github.com>
2025-06-26 16:45:13 +0300

    Revert "fix: refactor interactions (#115)"
    This reverts commit db88c942484347678ee06a2bd20990234963b092.

--- app/src/main/java/com/london/tudee/presentation/screens/task/TaskModifyInteractions.kt
+++ app/src/main/java/com/london/tudee/presentation/screens/task/TaskModifyInteractions.kt
@@ -5,9 +5,12 @@ import com.london.tudee.domain.entities.Priority
 
 interface TaskModifyInteractions {
     fun saveTask()
-    fun updateTitle(title: String)
-    fun updateDescription(description: String)
+    fun showDatePicker()
+    fun hideDatePicker()
+    fun hideBottomSheet()
     fun updateDate(date: Long)
+    fun updateTitle(title: String)
     fun updatePriority(priority: Priority)
     fun updateCategory(category: Category)
+    fun updateDescription(description: String)
 }
