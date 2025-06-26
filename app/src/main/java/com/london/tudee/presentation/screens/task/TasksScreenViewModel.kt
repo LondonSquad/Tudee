6590161c8c52dea95e7cd8a282d0f672bc7a71c0
Abdulrahman1890 <Abdulrahmanmm2011@hotmail.com>
2025-06-26 19:45:29 +0300

    fix: solve TasksScreenViewModel status conflict

--- app/src/main/java/com/london/tudee/presentation/screens/task/TasksScreenViewModel.kt
+++ app/src/main/java/com/london/tudee/presentation/screens/task/TasksScreenViewModel.kt
@@ -58,7 +58,7 @@ class TasksScreenViewModel(
 
             val (startOfDayMillis, endOfDayMillis) = TasksScreenUtils.getDayRangeMillis(targetDate)
             taskService.getTasksForDay(
-                start = startOfDayMillis, end = endOfDayMillis, taskStatus = TaskStatus.DONE
+                start = startOfDayMillis, end = endOfDayMillis, status = TaskStatus.DONE
             ).catch { throwable ->
                 _uiState.update {
                     it.copy(isLoading = false, errMessage = throwable.message)
@@ -83,7 +83,7 @@ class TasksScreenViewModel(
             val (startOfDayMillis, endOfDayMillis) = TasksScreenUtils.getDayRangeMillis(targetDate)
 
             taskService.getTasksForDay(
-                start = startOfDayMillis, end = endOfDayMillis, taskStatus = TaskStatus.IN_PROGRESS
+                start = startOfDayMillis, end = endOfDayMillis, status = TaskStatus.IN_PROGRESS
             ).catch { throwable ->
                 _uiState.update {
                     it.copy(isLoading = false, errMessage = throwable.message)
@@ -107,7 +107,7 @@ class TasksScreenViewModel(
             val targetDate = _uiState.value.date.toLocalDate()
             val (startOfDayMillis, endOfDayMillis) = TasksScreenUtils.getDayRangeMillis(targetDate)
             taskService.getTasksForDay(
-                start = startOfDayMillis, end = endOfDayMillis, taskStatus = TaskStatus.TODO
+                start = startOfDayMillis, end = endOfDayMillis, status = TaskStatus.TODO
             ).catch { throwable ->
                 _uiState.update {
                     it.copy(isLoading = false, errMessage = throwable.message)
