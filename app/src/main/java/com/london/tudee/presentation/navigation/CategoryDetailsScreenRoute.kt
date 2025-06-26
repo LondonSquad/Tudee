3d89b72eed9d6cc523e83055bb486c6fcb27c79a
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-06-26 16:19:42 +0300

    fix: replace popBackStack with navigateUp

--- app/src/main/java/com/london/tudee/presentation/navigation/CategoryDetailsScreenRoute.kt
+++ app/src/main/java/com/london/tudee/presentation/navigation/CategoryDetailsScreenRoute.kt
@@ -14,7 +14,7 @@ fun NavGraphBuilder.categoryDetailsRoute(navController: NavController) {
         CategoryDetailsScreen(
             categoryId = categoryId,
             onBackClick = {
-                navController.popBackStack()
+                navController.navigateUp()
             }
         )
     }
