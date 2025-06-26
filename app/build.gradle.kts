27d8090b54cf55bbf39053aed12177bc813ea1d8
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-06-26 21:02:52 +0300

    feat: implement splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,8 @@ dependencies {
     testImplementation (libs.truth)
     testImplementation(kotlin("test"))
 
+    // Splash Screen
+    implementation(libs.androidx.splashscreen)
 }
 
 tasks.register("installGitHooks") {
