dfc0edaea63a8ac61a8ebef53431cc975516b2c8
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-06-26 18:53:28 +0300

    test/categories test case (#122)
    * feat: add CategoriesServicesImpl unit test and exceptions
    
    * feat: add CategoriesServicesImpl unit test and exceptions
    
    * feat: add kotlin test implementation in build.gradle.kts
    
    * fix :conflict
    
    * fix :error in name of task dao
    
    * refactor: update CategoriesServicesImplTest incorrect import
    
    * refactor: replace Assertions with assertEquals
    
    * refactor: simplify exception handling in services

---
+++ app/src/test/java/com/london/tudee/data/mappers/CategoryMappersTest.kt
@@ -0,0 +1,32 @@
+package com.london.tudee.data.mappers
+
+import com.london.tudee.data.local.roomdb.dto.CategoryDto
+import com.london.tudee.domain.entities.Category
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CategoryMappersTest {
+    @Test
+    fun `when convert CategoryDto to convertToCategory returns Category`() {
+        //Given & When
+        val result = testCategoryDto().convertToCategory()
+        //Then
+       assertEquals(result, testCategory())
+    }
+
+    @Test
+    fun `when convert Category to convertToCategoryDto returns CategoryDto`() {
+        //Given & When
+        val result = testCategory().convertToCategoryDto()
+        //Then
+        assertEquals(result, testCategoryDto())
+    }
+
+    private fun testCategoryDto() = CategoryDto(
+        id = 1, title = "Work", isDefault = true, iconRes = "ic_work", taskCount = 5
+    )
+
+    private fun testCategory() = Category(
+        id = 1, title = "Work", isDefault = true, iconRes = "ic_work", taskCount = 5
+    )
+}
